dc87530bc6388b08814be737d3fa6fd206bcfe75
DESKTOP-JUSC5N8\Administrator <dev.greact@gmail.com>
2023-09-18 20:02:57 +0900

    SEO 적용 1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>건강증진센터 위치 서비스</title>
+        <meta name="description" content="건강증진센터 위치 서비스" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
