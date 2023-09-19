68ad96cd43f2e39cbbde288416033f386ac0fff3
DESKTOP-JUSC5N8\Administrator <dev.greact@gmail.com>
2023-09-19 16:22:45 +0900

    네이버 서치 어드바이저 적용

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  titleTemplate: '%s - 페이지',
+  titleTemplate: '%s - 서비스 페이지',
   openGraph: {
     type: 'website',
     site_name: '건강증진센터 위치보기 서비스',
@@ -12,6 +12,12 @@ export default {
     {
       rel: 'shortcut icon',
       href: '/favicon.ico',
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: 'naver-site-verification',
+      content: '386a8c1f48c3bf9c492898e78e52fdf6a0a66b2d',
     },
   ],
 };
